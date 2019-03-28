89fc913b6d66064c0ce3491a399c0756d8d33a17
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2019-03-28 15:25:34 +0200

    Add logos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import asteriskilogo from "./public/Asteriski_ry_logo_2017_kelt.png";
+import reaktorlogo from "./public/Reaktor_Logo_NeonRed_RGB.png";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={asteriskilogo} className="App-logo" alt="logo" />
+          <span style={{ color: "#FFFFFF", marginLeft: "1em" }}>X</span>
+          <img src={reaktorlogo} style={{ marginLeft: "1em" }} className="App-logo" alt="logo" />
         </header>
       </div>
     );
