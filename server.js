0db903fba8dd4e5d463978fe1edd771bdf6110ce
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2019-03-29 13:56:15 +0200

    Task 7

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const express = require("express");
 const app = express();
 const port = 3000;
 
+const events = JSON.parse(fs.readFileSync(path.resolve('./src/events.json')));
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/events", (req, res) => res.json(events));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
