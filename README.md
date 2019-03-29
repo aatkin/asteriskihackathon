f982a088c12f20b76517dda662e420c500814dbe
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2019-03-29 13:17:39 +0200

    Task 4

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Asteriski x Reaktor workshop 29.3.2019
 
 ## TODO
- - [ ] Refactor event list to show only three upcoming events (using method of your choice, e.g. `ramda`)
- - [ ] Create `<Events />` component to render upcoming events and make the list look nicer `:)`
+ - [ ] Create `<TimeView />` component that display current time and date
--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
-import { format } from "date-fns";
+import { format, isAfter, isWithinRange } from "date-fns";
+import { compose, filter, take } from "ramda";
 import asteriskilogo from "./public/Asteriski_ry_logo_2017_kelt.png";
 import reaktorlogo from "./public/Reaktor_Logo_NeonRed_RGB.png";
 import "./App.css";
 
 import events from "./events.json";
 
-const mapEvent = event => {
-  const startTime = format(new Date(event.startTime), "HH.mm");
-  return `${event.name} ${startTime}`;
-};
+const isUpcomingEvent = event => isAfter(event.endTime, new Date());
+const getVisibleEvents = compose(
+  take(3),
+  filter(isUpcomingEvent)
+);
+const getEventClassName = event => (isWithinRange(Date.now(), event.startTime, event.endTime) ? "event event--active" : "event");
+
+const Events = props => (
+  <div className="App-events">
+    <div className="App-events__eventNameContainer">
+      {getVisibleEvents(props.events).map((event, index) => (
+        <p key={index} style={{ textAlign: "left" }} className={getEventClassName(event)}>
+          {event.name}
+        </p>
+      ))}
+    </div>
+    <div className="App-events__eventDateContainer">
+      {getVisibleEvents(props.events).map((event, index) => (
+        <p key={index + "i"} className={getEventClassName(event)}>
+          {format(event.startTime, "HH.mm")}
+        </p>
+      ))}
+    </div>
+  </div>
+);
 
 class App extends Component {
   render() {
@@ -20,11 +42,7 @@ class App extends Component {
           <span style={{ marginLeft: "1em" }}>X</span>
           <img src={reaktorlogo} className="App-logo" style={{ marginLeft: "1em" }} alt="logo" />
         </header>
-        <div className="App-events">
-          {events.map((event, index) => (
-            <p key={index}>{mapEvent(event)}</p>
-          ))}
-        </div>
+        <Events events={events} />
       </div>
     );
   }
